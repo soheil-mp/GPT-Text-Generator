8dbb5806602a72efae52a1705109c12b141b57ff
Soheil <Soheil.mpg@gmail.com>
2023-05-17 11:04:22 +0400

    update

--- train.py
+++ train.py
@@ -1,149 +1,74 @@
-
-# Import the libraries
-import re, os, string, random
-import numpy as np
 import tensorflow as tf
+from tensorflow import keras
 
-# Causal attention mask function
-def causal_attention_mask(batch_size, n_dest, n_src, dtype):
-    """
-    Mask the future tokens to ensure that the attention mechanism only uses 
-    information from current and previous tokens.
-    """
-
-    # Initialize the indices for i and j
-    i, j = tf.range(n_dest)[:, None], tf.range(n_src)
-
-    # Mask
-    mask = i >= (j - n_src + n_dest)
-
-    # Expand the mask
+def causal_attention_mask(batch_size, dest_seq_len, src_seq_len, dtype):
+    i, j = tf.range(dest_seq_len)[:, None], tf.range(src_seq_len)
+    mask = i >= (j - src_seq_len + dest_seq_len)
     mask = tf.cast(mask, dtype)
-
-    # Reshape the mask
-    mask = tf.reshape(mask, [1, n_dest, n_src])
-
-    # Only keep the upper half of the dot prodict matrix
+    mask = tf.reshape(mask, [1, dest_seq_len, src_seq_len])
     matrix_mult = tf.concat([tf.expand_dims(batch_size), tf.constant([1, 1], dtype=tf.int32)], axis=0)
-
-    # Tile the mask
     out = tf.tile(mask, matrix_mult)
-
     return out
 
-# Transforme block class
 class TransformerBlock(tf.keras.layers.Layer):
-
-    # Constructor function
-    def __init__(self, embed_dim, num_heads, ff_dims, rate):
-
-        # Inherite the parent's constructor
+    def __init__(self, embed_dim, num_heads, feed_forward_dim, dropout_rate):
         super(TransformerBlock, self).__init__()
-
-        # Self-attention layer
-        self.att = tf.keras.layers.MultiHeadAttention(num_heads, embed_dim)
-
-        # FFN layer
-        self.ffn = tf.keras.Sequential([
-            tf.keras.layers.Dense(ff_dims, activation="relu"),
+        self.multi_head_attention = tf.keras.layers.MultiHeadAttention(num_heads, embed_dim)
+        self.feed_forward_network = tf.keras.Sequential([
+            tf.keras.layers.Dense(feed_forward_dim, activation="relu"),
             tf.keras.layers.Dense(embed_dim)
         ])
+        self.layer_norm1 = tf.keras.layers.LayerNormalization(epsilon=1e-6)
+        self.layer_norm2 = tf.keras.layers.LayerNormalization(epsilon=1e-6)
+        self.dropout1 = tf.keras.layers.Dropout(dropout_rate)
+        self.dropout2 = tf.keras.layers.Dropout(dropout_rate)
 
-        # Layer normalization
-        self.layernorm1 = tf.keras.layers.LayerNormalization(epsilon=1e-6)
-        self.layernorm2 = tf.keras.layers.LayerNormalization(epsilon=1e-6)
-
-        # Dropout layer
-        self.dropout1 = tf.keras.layers.Dropout(rate)
-        self.dropout2 = tf.keras.layers.Dropout(rate)
-
-    # Call function
-    def call(self, inputs, training, ):
-
-        # Initialize th variables
+    def call(self, inputs, training):
         input_shape = tf.shape(inputs)
         batch_size = input_shape[0]
         seq_len = input_shape[1]
         causal_mask = causal_attention_mask(batch_size, seq_len, seq_len, tf.bool)
-
-        # First attention block
-        attention_output = self.att(inputs, inputs, attention_mask=causal_mask)
+        attention_output = self.multi_head_attention(inputs, inputs, attention_mask=causal_mask)
         attention_output = self.dropout1(attention_output, training=training)
-        out1 = self.layernorm1(inputs + attention_output)
-
-        # Second attention block
-        ffn_output = self.ffn(out1)
+        out1 = self.layer_norm1(inputs + attention_output)
+        ffn_output = self.feed_forward_network(out1)
         ffn_output = self.dropout2(ffn_output, training=training)
         out = self.layer_norm2(out1 + ffn_output)
-
         return out
-    
-# Token and positional embedding class
+
 class TokenAndPositionEmbedding(tf.keras.layers.Layer):
-
-    # Initialize the constructor function
-    def __init__(self, maxlen, vocab_size, embed_dim):
-
-        # Inherite the parent's constructorr
+    def __init__(self, max_seq_len, vocab_size, embed_dim):
         super(TokenAndPositionEmbedding, self).__init__()
+        self.token_embedding = tf.keras.layers.Emebdding(input_dim=vocab_size, output_dim=embed_dim)
+        self.position_embedding = tf.keras.layers.Embedding(input_dim=max_seq_len, output_dim=embed_dim)
 
-        # Token embedding
-        self.token_embed= tf.keras.layers.Emebdding(input_dim=vocab_size, output_dim=embed_dim)
-
-        # Position embedding layer
-        self.pos_emb = tf.keras.layers.Embedding(input_dim=maxlen, output_dim=embed_dim)
-
-    # Call function
     def call(self, x):
-
-        # Maximum sequence length
-        maxlen = tf.shape(x)[-1]
-
-        # Initialize the positions
-        positions = tf.range(start=0, limit=maxlen, delta=1)
-
-        # Feed the positions to the position embedding layer
-        positions = self.pos_emb(positions)
-
-        # Feed the tokens tot he token emebedding layer
-        x = self.token_embed(x)
-
-        # Add the token and position embedding
+        max_seq_len = tf.shape(x)[-1]
+        positions = tf.range(start=0, limit=max_seq_len, delta=1)
+        positions = self.position_embedding(positions)
+        x = self.token_embedding(x)
         out = x + positions
-
         return out
-    
-# Initialize the hyperparameters
+
 vocab_size = 20000
-maxlen = 80
+max_seq_len = 80
 embed_dim = 256
 num_heads = 2
 feed_forward_dim = 256
+dropout_rate = 0.1
 
-# GPT model
-def create_model():
-
-    # Input layer 
-    inputs = tf.keras.layers.Input(shape=(maxlen, ), dtype=tf.int32)
-
-    # Token and position embedding layer
-    embedding_layer = TokenAndPositionEmbedding(maxlen, vocab_size, embed_dim)
+def create_transformer_model():
+    inputs = tf.keras.layers.Input(shape=(max_seq_len, ), dtype=tf.int32)
+    embedding_layer = TokenAndPositionEmbedding(max_seq_len, vocab_size, embed_dim)
     x = embedding_layer(inputs)
-
-    # Transformer blck
-    transformer_block = TransformerBlock(embed_dim, num_heads, feed_forward_dim)
+    transformer_block = TransformerBlock(embed_dim, num_heads, feed_forward_dim, dropout_rate)
     x = transformer_block(x)
-
-    # Output layer
     outputs = tf.keras.layers.Dense(vocab_size)(x)
-
-    # Construct the model
-    model = tf.keras.Model(inputs=inputs, outputs=[outputs, x])
-
-    # Loss function
     loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+    model = tf.keras.Model(inputs=inputs, outputs=[outputs, x])
+    model.compile("adam", loss=[loss_fn, None])
+    return model
 
-    # Compile the model
-    model.compile("adam", loss=[loss_fn, None], )
-
-    return model 
+# Initialize the model
+model = create_transformer_model()
+print(model.summary())
